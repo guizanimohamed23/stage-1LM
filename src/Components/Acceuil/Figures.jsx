d6a13e1f829c23d2227135c9df253108e58df84e
guizanimohamed23 <guizanimohamed91@gmail.com>
2024-09-09 18:13:05 +0100

    add animation

--- src/Components/Acceuil/Figures.jsx
+++ src/Components/Acceuil/Figures.jsx
@@ -1,48 +1,194 @@
-import * as React from 'react';
-import { Box, Typography } from '@mui/material';
-import Grid from '@mui/material/Grid2';
-import EditCalendarIcon from '@mui/icons-material/EditCalendar';
-import CheckIcon from '@mui/icons-material/Check';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import { useTranslation } from 'react-i18next';
+import * as React from "react";
+import { Box, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import EditCalendarIcon from "@mui/icons-material/EditCalendar";
+import CheckIcon from "@mui/icons-material/Check";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import { useTranslation } from "react-i18next";
 
 function Figures() {
   const { t } = useTranslation();
 
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '5rem', marginBottom: '5rem' }}>
-      <Box bgcolor={"#1A4870"} color={'white'} borderRadius={'2rem'} padding={'2.5rem'}>
-        <Grid container xs={12} spacing={10} sx={{ display: 'flex', justifyContent: 'center' }}>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        marginTop: "5rem",
+        marginBottom: "5rem",
+      }}
+    >
+      <Box
+        bgcolor={"#1A4870"}
+        color={"white"}
+        borderRadius={"2rem"}
+        padding={"2.5rem"}
+      >
+        <Grid
+          container
+          xs={12}
+          spacing={10}
+          sx={{ display: "flex", justifyContent: "center" }}
+        >
           <Grid item xs={2} sm={2}>
             <Grid container spacing={2}>
-              <Grid item sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <EditCalendarIcon sx={{ fontSize: '3rem' }} />
+              <Grid
+                item
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <EditCalendarIcon
+                  sx={{
+                    fontSize: "3rem",
+                    color: "white",
+                    transition: "transform 0.3s ease, color 0.3s ease",
+                    "&:hover": {
+                      transform: "scale(1.2)", // "Pop" effect
+                      color: "blue", // Color change on hover
+                    },
+                  }}
+                />
               </Grid>
               <Grid item>
-                <Typography variant='h5'>{t('figures.avis')}</Typography>
-                <Typography variant='h5' color="#EABA2B">10</Typography>
+                <Typography
+                  variant="h5"
+                  sx={{
+                    fontSize: "24px",
+                    color: "white",
+                    transition: "transform 0.3s ease, color 0.3s ease",
+                    "&:hover": {
+                      transform: "scale(1.2)", // "Pop" effect
+                      color: "blue", // Color change on hover
+                    },
+                  }}
+                >
+                  {t("figures.avis")}
+                </Typography>
+                <Typography
+                  variant="h5"
+                  color="#EABA2B"
+                  sx={{
+                    fontSize: "24px",
+                    transition: "transform 0.3s ease, color 0.3s ease",
+                    "&:hover": {
+                      transform: "scale(1.2)", // "Pop" effect
+                      color: "#FF7B07", // Color change on hover
+                    },
+                  }}
+                >
+                  10
+                </Typography>
               </Grid>
             </Grid>
           </Grid>
           <Grid item xs={2} sm={2}>
             <Grid container spacing={2}>
-              <Grid item sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <CheckIcon sx={{ fontSize: '3rem' }} />
+              <Grid
+                item
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <CheckIcon
+                  sx={{
+                    fontSize: "3rem",
+                    color: "white",
+                    transition: "transform 0.3s ease, color 0.3s ease",
+                    "&:hover": {
+                      transform: "scale(1.2)", // "Pop" effect
+                      color: "blue", // Color change on hover
+                    },
+                  }}
+                />
               </Grid>
               <Grid item>
-                <Typography variant='h5'>{t('figures.decisions')}</Typography>
-                <Typography variant='h5' color="#EABA2B">11</Typography>
+                <Typography
+                  variant="h5"
+                  sx={{
+                    fontSize: "24px",
+                    color: "white",
+                    transition: "transform 0.3s ease, color 0.3s ease",
+                    "&:hover": {
+                      transform: "scale(1.2)", // "Pop" effect
+                      color: "blue", // Color change on hover
+                    },
+                  }}
+                >
+                  {t("figures.decisions")}
+                </Typography>
+                <Typography
+                  variant="h5"
+                  color="#EABA2B"
+                  sx={{
+                    fontSize: "24px",
+                    transition: "transform 0.3s ease, color 0.3s ease",
+                    "&:hover": {
+                      transform: "scale(1.2)", // "Pop" effect
+                      color: "#FF7B07", // Color change on hover
+                    },
+                  }}
+                >
+                  11
+                </Typography>
               </Grid>
             </Grid>
           </Grid>
           <Grid item xs={2} sm={2}>
             <Grid container spacing={2}>
-              <Grid item sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <BarChartIcon sx={{ fontSize: '3rem' }} />
+              <Grid
+                item
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <BarChartIcon
+                  sx={{
+                    fontSize: "3rem",
+                    color: "white",
+                    transition: "transform 0.3s ease, color 0.3s ease",
+                    "&:hover": {
+                      transform: "scale(1.2)", // "Pop" effect
+                      color: "blue", // Color change on hover
+                    },
+                  }}
+                />
               </Grid>
               <Grid item>
-                <Typography variant='h5'>{t('figures.reports')}</Typography>
-                <Typography variant='h5' color="#EABA2B">13</Typography>
+                <Typography
+                  variant="h5"
+                  sx={{
+                    fontSize: "24px",
+                    color: "white",
+                    transition: "transform 0.3s ease, color 0.3s ease",
+                    "&:hover": {
+                      transform: "scale(1.2)", // "Pop" effect
+                      color: "blue", // Color change on hover
+                    },
+                  }}
+                >
+                  {t("figures.reports")}
+                </Typography>
+                <Typography
+                  variant="h5"
+                  color="#EABA2B"
+                  sx={{
+                    fontSize: "24px",
+                    transition: "transform 0.3s ease, color 0.3s ease",
+                    "&:hover": {
+                      transform: "scale(1.1)", // "Pop" effect
+                      color: "#FF7B07", // Color change on hover
+                    },
+                  }}
+                >
+                  13
+                </Typography>
               </Grid>
             </Grid>
           </Grid>
--- src/screens/Information.jsx
+++ src/screens/Information.jsx
@@ -1,216 +1,231 @@
+import React, { useState, useEffect } from "react";
 import { Box, Button, TextField, Typography, Divider } from "@mui/material";
 import Footer from "../Components/Footer";
 import Navbar from "../Components/Navbar";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import Grid from "@mui/material/Grid2";
 import cover from "/image 3.png";
-import { textAlign } from "@mui/system";
+import Fade from "@mui/material/Fade";
 
 function Information() {
+  const [fadeIn, setFadeIn] = useState(false);
+
+  useEffect(() => {
+    setFadeIn(true);
+  }, []);
+
   return (
     <Box sx={{ overflowX: "hidden" }}>
       {/* Navbar Component */}
       <Navbar />
 
       {/* Main Content */}
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          padding: "1rem",
-          marginTop: "5rem",
-        }}
-      >
+      <Fade in={fadeIn} timeout={1000}>
         <Box
-          bgcolor={"#f7f9fa"}
-          borderRadius={"1rem"}
-          padding={"2rem"}
-          width={{ xs: "90%", sm: "80%", md: "70%" }} // Responsive width
-          boxShadow={"0 4px 8px rgba(0, 0, 0, 0.1)"}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            padding: "1rem",
+            marginTop: "5rem",
+          }}
         >
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <Typography
-                variant="h4"
-                color={"#1A4870"}
-                sx={{ fontWeight: "bold", marginBottom: "1rem" }}
-              >
-                LE DROIT D'ACCÈS À L'INFORMATION
-              </Typography>
-              <Divider
-                sx={{
-                  width: { xs: "90%", sm: "80%", md: "660px" },
-                  borderWidth: "3px",
-                  borderRadius: ".5rem",
-                  bgcolor: "#EABA2B",
-                  marginBottom: "1rem",
-                }}
-              />
-              <Typography
-                sx={{
-                  marginBottom: "1.5rem",
-                  color: "#1A4870",
-                  fontWeight: "bold",
-                }}
-              >
-                LE DROIT D'ACCÈS À L'INFORMATION (DAI) EST UN DROIT FONDAMENTAL
-                DE L'INDIVIDU ET DE LA COLLECTIVITÉ QUI EST ASSURÉ PAR LA :
-              </Typography>
+          <Box
+            borderRadius={"1rem"}
+            padding={"2rem"}
+            width={{ xs: "90%", sm: "80%", md: "70%" }} // Responsive width
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Box
+                  sx={{
+                    display: "inline-block",
+                    padding: "10px",
+                    transition: "box-shadow 1s ease",
+                    borderRadius: "20px",
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      fontSize: "20px",
+                      marginBottom: "1.5rem",
+                      color: "#1A4870",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    LE DROIT D'ACCÈS À L'INFORMATION (DAI) EST UN DROIT
+                    FONDAMENTAL DE L'INDIVIDU ET DE LA COLLECTIVITÉ QUI EST
+                    ASSURÉ PAR LA :
+                  </Typography>
 
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <Grid
-                    container
-                    alignItems="center"
-                    sx={{ marginBottom: "1rem" }}
-                  >
-                    <Box display="flex" alignItems="center">
-                      <CheckCircleIcon
-                        sx={{ color: "#5B99C2", marginRight: "0.5rem" }}
-                      />
-                      <Typography
-                        sx={{
-                          color: "#3A3A3A",
-                          fontSize: "1.1rem",
-                        }}
-                      >
-                        Décret-loi n°41 du 26 mai 2011, relatif à l’accès aux
-                        documents administratifs des organismes publics, modifié
-                        et complété par le décret-loi n° 2011-54 du 11 juin
-                        2011.
-                      </Typography>
-                    </Box>
-                  </Grid>
-                  <Grid
-                    container
-                    alignItems="center"
-                    sx={{ marginBottom: "1rem" }}
-                  >
-                    <Box display="flex" alignItems="center">
-                      <CheckCircleIcon
-                        sx={{ color: "#5B99C2", marginRight: "0.5rem" }}
-                      />
-                      <Typography
-                        sx={{
-                          color: "#3A3A3A",
-                          fontSize: "1.1rem",
-                        }}
-                      >
-                        Loi organique n° 2016-22 du 24 mars 2016, relative au
-                        droit d’accès à l’information.
-                      </Typography>
-                    </Box>
-                  </Grid>
-                  <Grid container alignItems="center">
-                    <Box display="flex" alignItems="center">
-                      <CheckCircleIcon
-                        sx={{ color: "#5B99C2", marginRight: "0.5rem" }}
-                      />
-                      <Typography
-                        sx={{
-                          color: "#3A3A3A",
-                          fontSize: "1.1rem",
-                        }}
-                      >
-                        La Constitution du 27 Janvier 2014 Art. 32 : L’État
-                        garantit le droit à l’information et le droit d’accès à
-                        l’information.
-                      </Typography>
-                    </Box>
+                  <Divider
+                    sx={{
+                      width: "100%",
+                      borderWidth: "2px",
+                    }}
+                  />
+                </Box>
+
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <Grid
+                      container
+                      alignItems="center"
+                      sx={{ marginBottom: "1rem" }}
+                    >
+                      <Box display="flex" alignItems="center">
+                        <CheckCircleIcon
+                          sx={{ color: "#5B99C2", marginRight: "0.5rem" }}
+                        />
+                        <Typography
+                          sx={{
+                            color: "#3A3A3A",
+                            fontSize: "1.1rem",
+                          }}
+                        >
+                          Décret-loi n°41 du 26 mai 2011, relatif à l’accès aux
+                          documents administratifs des organismes publics,
+                          modifié et complété par le décret-loi n° 2011-54 du 11
+                          juin 2011.
+                        </Typography>
+                      </Box>
+                    </Grid>
+                    <Grid
+                      container
+                      alignItems="center"
+                      sx={{ marginBottom: "1rem" }}
+                    >
+                      <Box display="flex" alignItems="center">
+                        <CheckCircleIcon
+                          sx={{ color: "#5B99C2", marginRight: "0.5rem" }}
+                        />
+                        <Typography
+                          sx={{
+                            color: "#3A3A3A",
+                            fontSize: "1.1rem",
+                          }}
+                        >
+                          Loi organique n° 2016-22 du 24 mars 2016, relative au
+                          droit d’accès à l’information.
+                        </Typography>
+                      </Box>
+                    </Grid>
+                    <Grid container alignItems="center">
+                      <Box display="flex" alignItems="center">
+                        <CheckCircleIcon
+                          sx={{ color: "#5B99C2", marginRight: "0.5rem" }}
+                        />
+                        <Typography
+                          sx={{
+                            color: "#3A3A3A",
+                            fontSize: "1.1rem",
+                          }}
+                        >
+                          La Constitution du 27 Janvier 2014 Art. 32 : L’État
+                          garantit le droit à l’information et le droit d’accès
+                          à l’information.
+                        </Typography>
+                      </Box>
+                    </Grid>
                   </Grid>
                 </Grid>
-              </Grid>
-
-              <Typography
-                variant="h5"
-                color={"#1A4870"}
-                sx={{
-                  marginTop: "2rem",
-                  marginBottom: "1rem",
-                  fontWeight: "bold",
-                  fontSize: { xs: "1.5rem", md: "1.75rem" },
-                }}
-              >
-                AU SENS DE LA PRÉSENTE LOI EN ENTEND PAR LES TERMES SUIVANTS :
-              </Typography>
-              <Divider
-                sx={{
-                  width: { xs: "90%", sm: "80%", md: "600px", lg: "900px" },
-                  borderWidth: "3px",
-                  borderRadius: ".5rem",
-                  bgcolor: "#EABA2B",
-                  marginBottom: "1rem",
-                }}
-              />
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <Grid
-                    container
-                    alignItems="center"
-                    sx={{ marginBottom: "1rem" }}
+                <Box
+                  sx={{
+                    display: "inline-block",
+                    padding: "10px",
+                    transition: "box-shadow 1s ease",
+                    borderRadius: "20px",
+                  }}
+                >
+                  <Typography
+                    variant="h5"
+                    color={"#1A4870"}
+                    sx={{
+                      marginTop: "2rem",
+                      marginBottom: "1rem",
+                      fontWeight: "bold",
+                      fontSize: "20px",
+                    }}
                   >
-                    <Box display="flex" alignItems="center">
-                      <CheckCircleIcon
-                        sx={{ color: "#5B99C2", marginRight: "0.5rem" }}
-                      />
-                      <Typography
-                        sx={{
-                          color: "#3A3A3A",
-                          fontSize: "1.1rem",
-                        }}
-                      >
-                        L'accès à l'information : la publication proactive de
-                        l'information par l'organisme concerné et le droit d'y
-                        accéder sur demande.
-                      </Typography>
-                    </Box>
-                  </Grid>
-                  <Grid
-                    container
-                    alignItems="center"
-                    sx={{ marginBottom: "1rem" }}
-                  >
-                    <Box display="flex" alignItems="center">
-                      <CheckCircleIcon
-                        sx={{ color: "#5B99C2", marginRight: "0.5rem" }}
-                      />
-                      <Typography
-                        sx={{
-                          color: "#3A3A3A",
-                          fontSize: "1.1rem",
-                        }}
-                      >
-                        Information : toute information enregistrée quelque soit
-                        sa date, sa forme et son support, produite ou obtenue
-                        par les organismes soumis aux dispositions de la
-                        présente loi dans le cadre de l'exercice de leurs
-                        activités.
-                      </Typography>
-                    </Box>
-                  </Grid>
-                  <Grid container alignItems="center">
-                    <Box display="flex" alignItems="center">
-                      <CheckCircleIcon
-                        sx={{ color: "#5B99C2", marginRight: "0.5rem" }}
-                      />
-                      <Typography
-                        sx={{
-                          color: "#3A3A3A",
-                          fontSize: "1.1rem",
-                        }}
-                      >
-                        Le tiers : toute personne physique ou morale autre que
-                        l'organisme concerné détenteur de l'information et le
-                        demandeur d'accès à l'information.
-                      </Typography>
-                    </Box>
+                    AU SENS DE LA PRÉSENTE LOI EN ENTEND PAR LES TERMES SUIVANTS
+                    :
+                  </Typography>
+                  <Divider
+                    sx={{
+                      width: "100%",
+                      borderWidth: "2px",
+                    }}
+                  />
+                </Box>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <Grid
+                      container
+                      alignItems="center"
+                      sx={{ marginBottom: "1rem" }}
+                    >
+                      <Box display="flex" alignItems="center">
+                        <CheckCircleIcon
+                          sx={{ color: "#5B99C2", marginRight: "0.5rem" }}
+                        />
+                        <Typography
+                          sx={{
+                            color: "#3A3A3A",
+                            fontSize: "1.1rem",
+                          }}
+                        >
+                          L'accès à l'information : la publication proactive de
+                          l'information par l'organisme concerné et le droit d'y
+                          accéder sur demande.
+                        </Typography>
+                      </Box>
+                    </Grid>
+                    <Grid
+                      container
+                      alignItems="center"
+                      sx={{ marginBottom: "1rem" }}
+                    >
+                      <Box display="flex" alignItems="center">
+                        <CheckCircleIcon
+                          sx={{ color: "#5B99C2", marginRight: "0.5rem" }}
+                        />
+                        <Typography
+                          sx={{
+                            color: "#3A3A3A",
+                            fontSize: "1.1rem",
+                          }}
+                        >
+                          Information : toute information enregistrée quelque
+                          soit sa date, sa forme et son support, produite ou
+                          obtenue par les organismes soumis aux dispositions de
+                          la présente loi dans le cadre de l'exercice de leurs
+                          activités.
+                        </Typography>
+                      </Box>
+                    </Grid>
+                    <Grid container alignItems="center">
+                      <Box display="flex" alignItems="center">
+                        <CheckCircleIcon
+                          sx={{ color: "#5B99C2", marginRight: "0.5rem" }}
+                        />
+                        <Typography
+                          sx={{
+                            color: "#3A3A3A",
+                            fontSize: "1.1rem",
+                          }}
+                        >
+                          Le tiers : toute personne physique ou morale autre que
+                          l'organisme concerné détenteur de l'information et le
+                          demandeur d'accès à l'information.
+                        </Typography>
+                      </Box>
+                    </Grid>
                   </Grid>
                 </Grid>
               </Grid>
             </Grid>
-          </Grid>
+          </Box>
         </Box>
-      </Box>
+      </Fade>
 
       {/* Background Box with Text Input and Button */}
       <Box
@@ -258,7 +273,7 @@ function Information() {
                     borderWidth: "2px", // Border width
                     borderColor: "white",
                     backgroundColor: "#1291FF",
-                    position: "relatiive",
+                    position: "relative",
                     right: 0,
                     top: 0,
                     height: "100%",
